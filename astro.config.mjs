0771a9e419bf639efabe64787ec9aa1c12767058
JonathanArroyaveGonzalez <arroyave3000g@gmail.com>
2024-03-15 21:25:49 -0500

    settings deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://JonathanArroyaveGonzalez.github.io',
+  base: '/Elicitacion-platzi',
+})
