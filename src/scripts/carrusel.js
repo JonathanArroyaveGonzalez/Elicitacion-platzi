9e2f16314f3eef1da31b8e5641593ebc2d074db7
JonathanArroyaveGonzalez <arroyave3000g@gmail.com>
2024-03-17 02:28:51 -0500

    Adición indice

---
+++ src/scripts/carrusel.js
@@ -0,0 +1,21 @@
+let slideIndex = 0;
+
+function showSlides() {
+  const slides = document.querySelectorAll('.slides img');
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  setTimeout(showSlides, 4000); // Cambia la imagen cada 2 segundos
+}
+
+function changeSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+showSlides();
+
